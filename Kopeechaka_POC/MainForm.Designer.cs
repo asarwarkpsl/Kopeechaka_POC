fb3b958253665ae0e3c23e5468d8fd6c9efe7d41
Azeem Sarwar <asarwar.kpsl@gmail.com>
2022-12-01 22:13:53 +0500

    Add project files.

---
+++ Kopeechaka_POC/MainForm.Designer.cs
@@ -0,0 +1,331 @@
+﻿namespace Kopeechaka_POC
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbServer = new System.Windows.Forms.ComboBox();
+            this.cmbEndPoint = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtSubject = new System.Windows.Forms.TextBox();
+            this.txtType = new System.Windows.Forms.TextBox();
+            this.txtRegx = new System.Windows.Forms.TextBox();
+            this.txtSite = new System.Windows.Forms.TextBox();
+            this.txtToken = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.lblOutPut = new System.Windows.Forms.Label();
+            this.btnExe = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(33, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Server";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(369, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "API(EndPoint)";
+            // 
+            // cmbServer
+            // 
+            this.cmbServer.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbServer.FormattingEnabled = true;
+            this.cmbServer.Items.AddRange(new object[] {
+            "https://api.kopeechka.store"});
+            this.cmbServer.Location = new System.Drawing.Point(33, 62);
+            this.cmbServer.Name = "cmbServer";
+            this.cmbServer.Size = new System.Drawing.Size(323, 28);
+            this.cmbServer.TabIndex = 0;
+            // 
+            // cmbEndPoint
+            // 
+            this.cmbEndPoint.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbEndPoint.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.cmbEndPoint.FormattingEnabled = true;
+            this.cmbEndPoint.Items.AddRange(new object[] {
+            "/Balance Request",
+            "/Reorder mail"});
+            this.cmbEndPoint.Location = new System.Drawing.Point(369, 62);
+            this.cmbEndPoint.Name = "cmbEndPoint";
+            this.cmbEndPoint.Size = new System.Drawing.Size(320, 28);
+            this.cmbEndPoint.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cmbEndPoint);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.cmbServer);
+            this.groupBox1.Location = new System.Drawing.Point(28, 26);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(887, 131);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "API Details";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtEmail);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.txtSubject);
+            this.groupBox2.Controls.Add(this.txtType);
+            this.groupBox2.Controls.Add(this.txtRegx);
+            this.groupBox2.Controls.Add(this.txtSite);
+            this.groupBox2.Controls.Add(this.txtToken);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Location = new System.Drawing.Point(28, 163);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(887, 274);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Input Params";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(166, 76);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(523, 27);
+            this.txtEmail.TabIndex = 4;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(91, 77);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(56, 20);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "$Email";
+            // 
+            // txtSubject
+            // 
+            this.txtSubject.Location = new System.Drawing.Point(166, 237);
+            this.txtSubject.Name = "txtSubject";
+            this.txtSubject.Size = new System.Drawing.Size(523, 27);
+            this.txtSubject.TabIndex = 8;
+            // 
+            // txtType
+            // 
+            this.txtType.Location = new System.Drawing.Point(166, 194);
+            this.txtType.Name = "txtType";
+            this.txtType.Size = new System.Drawing.Size(523, 27);
+            this.txtType.TabIndex = 7;
+            // 
+            // txtRegx
+            // 
+            this.txtRegx.Location = new System.Drawing.Point(166, 153);
+            this.txtRegx.Name = "txtRegx";
+            this.txtRegx.Size = new System.Drawing.Size(523, 27);
+            this.txtRegx.TabIndex = 6;
+            // 
+            // txtSite
+            // 
+            this.txtSite.Location = new System.Drawing.Point(166, 109);
+            this.txtSite.Name = "txtSite";
+            this.txtSite.Size = new System.Drawing.Size(523, 27);
+            this.txtSite.TabIndex = 5;
+            // 
+            // txtToken
+            // 
+            this.txtToken.Location = new System.Drawing.Point(166, 38);
+            this.txtToken.Name = "txtToken";
+            this.txtToken.Size = new System.Drawing.Size(523, 27);
+            this.txtToken.TabIndex = 3;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(91, 197);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 20);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "$Type";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label9.Location = new System.Drawing.Point(91, 240);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(69, 20);
+            this.label9.TabIndex = 5;
+            this.label9.Text = "$Subject";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(91, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 20);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "$Token";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(91, 156);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 20);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "$Regex";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(91, 112);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 20);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "$Site";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.lblOutPut);
+            this.groupBox3.Location = new System.Drawing.Point(32, 433);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(883, 214);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "OutPut";
+            // 
+            // lblOutPut
+            // 
+            this.lblOutPut.AutoSize = true;
+            this.lblOutPut.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblOutPut.Location = new System.Drawing.Point(36, 35);
+            this.lblOutPut.Name = "lblOutPut";
+            this.lblOutPut.Size = new System.Drawing.Size(223, 20);
+            this.lblOutPut.TabIndex = 0;
+            this.lblOutPut.Text = "** Enter params & hit Execute **";
+            // 
+            // btnExe
+            // 
+            this.btnExe.Location = new System.Drawing.Point(709, 672);
+            this.btnExe.Name = "btnExe";
+            this.btnExe.Size = new System.Drawing.Size(100, 45);
+            this.btnExe.TabIndex = 9;
+            this.btnExe.Text = "Execute";
+            this.btnExe.UseVisualStyleBackColor = true;
+            this.btnExe.Click += new System.EventHandler(this.btnExe_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(815, 672);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(100, 45);
+            this.btnClose.TabIndex = 10;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // MainForm
+            // 
+            this.AcceptButton = this.btnExe;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(938, 742);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnExe);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.RightToLeftLayout = true;
+            this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_LoadAsync);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private ComboBox cmbServer;
+        private ComboBox cmbEndPoint;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private TextBox txtSubject;
+        private TextBox txtType;
+        private TextBox txtRegx;
+        private TextBox txtSite;
+        private TextBox txtToken;
+        private Label label7;
+        private Label label9;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private GroupBox groupBox3;
+        private Label lblOutPut;
+        private Button btnExe;
+        private Button btnClose;
+        private TextBox txtEmail;
+        private Label label8;
+    }
+}
---
+++ Kopeechaka_POC/MainForm.cs
@@ -0,0 +1,95 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Security.Policy;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace Kopeechaka_POC
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_LoadAsync(object sender, EventArgs e)
+        {
+            ////// token: be62e73382087beb2738085377f40208
+
+            cmbServer.SelectedIndex = 0;
+            cmbEndPoint.SelectedIndex = 0;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnExe_Click(object sender, EventArgs e)
+        {
+             string urlParameters="";
+            string url = string.Empty;
+            HTTPResponse? jsonResult = null;
+
+            if (cmbEndPoint.SelectedIndex == 0)
+                url = $@"https://api.kopeechka.store/user-balance?token={txtToken.Text.Trim()}&type=json&api=2.0";
+            else if (cmbEndPoint.SelectedIndex == 1)
+                url = $@"https://api.kopeechka.store/mailbox-reorder?site={txtSite.Text.Trim()}&email={txtEmail.Text.Trim()}&regex={txtRegx.Text.Trim()}&token={txtToken.Text.Trim()}&type=json&subject={txtSubject.Text.Trim()}&api=2.0";
+
+            using var client = new HttpClient();
+            client.BaseAddress = new Uri(url);
+            // Add an Accept header for JSON format.
+            client.DefaultRequestHeaders.Accept.Add(
+               new MediaTypeWithQualityHeaderValue("application/json"));
+            // Get data response
+            var response = client.GetAsync(urlParameters).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var result = response.Content.ReadAsStringAsync().Result;
+
+                //JsonSerializer.Deserialize<deweyData>(result);
+
+                jsonResult = JsonConvert.DeserializeObject<HTTPResponse>(result);
+                //// Parse the response body
+                //var dataObjects = response.Content.ReadFromJsonAsync() .ReadAsAsync<IEnumerable<DataObject>>().Result;
+                //foreach (var d in dataObjects)
+                //{
+                //    Console.WriteLine("{0}", d.Name);
+                //}
+
+                if (cmbEndPoint.SelectedIndex == 0)
+                    lblOutPut.Text = jsonResult.balance != null ? jsonResult.balance : jsonResult.value;
+                else
+                    lblOutPut.Text = jsonResult.email!=null ? jsonResult.email : jsonResult.value;
+            }
+            else
+            {
+                //Console.WriteLine("{0} ({1})", (int)response.StatusCode,
+                //              response.ReasonPhrase);
+
+                lblOutPut.Text = jsonResult.value;
+            }
+        }
+    }
+
+    public class HTTPResponse
+    {
+        public string id { get; set; }
+        public string status { get; set; }
+        public string email { get; set; }
+        public string value { get; set; }
+        public string balance { get; set; }
+
+    }
+}
